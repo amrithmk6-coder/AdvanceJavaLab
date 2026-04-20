3fe9fbbd4c9f27edad1559dfeb2a31a13cde59dc
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 22:00:34 +0530

    Add files via upload

---
+++ RemoveWhitespace.java
@@ -0,0 +1,11 @@
+package nullempty;
+public class RemoveWhitespace {
+    static String removeWhitespace(String str) {
+        return str.replaceAll("\\s", "");
+    }
+
+    public static void main(String[] args) {
+        String str = "Java is easy to learn";
+        System.out.println("Without spaces: " + removeWhitespace(str));
+    }
+}
