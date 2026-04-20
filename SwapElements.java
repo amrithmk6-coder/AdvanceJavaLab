e5ffa045f01e2ebb32b9cc9a28deaf758ce756ef
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 21:31:13 +0530

    Add files via upload

---
+++ SwapElements.java
@@ -0,0 +1,19 @@
+package swapelements;
+
+import java.util.*;
+
+public class SwapElements {
+    public static void main(String[] args) {
+        LinkedList<String> l_list = new LinkedList<>();
+
+        l_list.add("Red");
+        l_list.add("Blue");
+        l_list.add("Green");
+        l_list.add("Yellow");
+
+        Collections.swap(l_list, 0, 2);
+
+        System.out.println("After swapping 1st and 3rd elements:");
+        System.out.println(l_list);
+    }
+}
