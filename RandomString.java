44fbee637409176f86fb5f3e16cb8ef7aa898eff
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 22:20:18 +0530

    Add files via upload

---
+++ RandomString.java
@@ -0,0 +1,20 @@
+package capital;
+
+import java.util.Random;
+
+public class RandomString {
+    static String generateRandomString(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random rand = new Random();
+        String result = "";
+
+        for (int i = 0; i < length; i++) {
+            result += chars.charAt(rand.nextInt(chars.length()));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateRandomString(8));
+    }
+}
