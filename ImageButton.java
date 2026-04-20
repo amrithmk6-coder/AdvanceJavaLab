a8e611c5f6a28e3017a7eafa571f0c410141b93c
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 22:46:19 +0530

    Add files via upload

---
+++ ImageButton.java
@@ -0,0 +1,41 @@
+package swing;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ImageButton {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Image Button");
+
+        JLabel l = new JLabel();
+        l.setBounds(100, 50, 300, 30);
+
+        ImageIcon clock = new ImageIcon("clock.png");
+        ImageIcon hourglass = new ImageIcon("hourglass.png");
+
+        JButton b1 = new JButton("Digital Clock", clock);
+        JButton b2 = new JButton("Hour Glass", hourglass);
+
+        b1.setBounds(50, 100, 200, 50);
+        b2.setBounds(50, 170, 200, 50);
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                l.setText("Digital Clock is pressed");
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                l.setText("Hour Glass is pressed");
+            }
+        });
+
+        f.add(l);
+        f.add(b1);
+        f.add(b2);
+
+        f.setSize(350, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
