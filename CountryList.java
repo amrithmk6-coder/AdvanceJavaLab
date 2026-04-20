a02461ea2e7082e78f0cc60543afcab5c4de96d6
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 23:02:49 +0530

    Add files via upload

---
+++ CountryList.java
@@ -0,0 +1,29 @@
+package swing2;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class CountryList {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Country List");
+
+        String countries[] = {"USA","India","Vietnam","Canada","Denmark",
+                              "France","Great Britain","Japan","Africa",
+                              "Greenland","Singapore"};
+
+        JList<String> list = new JList<>(countries);
+        list.setBounds(50, 50, 200, 200);
+
+        list.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                System.out.println("Selected: " + list.getSelectedValue());
+            }
+        });
+
+        f.add(list);
+        f.setSize(300, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
