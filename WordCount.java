5fb13974af4dba1ada131d5cdb7efec1597ca766
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 22:21:03 +0530

    Add files via upload

---
+++ WordCount.java
@@ -0,0 +1,16 @@
+package capital;
+
+public class WordCount {
+    static int countWords(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            return 0;
+        }
+        String[] words = str.trim().split("\\s+");
+        return words.length;
+    }
+
+    public static void main(String[] args) {
+        String str = "Java is easy to learn";
+        System.out.println("Word count: " + countWords(str));
+    }
+}
