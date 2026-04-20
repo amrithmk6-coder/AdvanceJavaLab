16d8f6a54b59971e8700a3cc57d009cc753de340
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 23:02:02 +0530

    Add files via upload

---
+++ ColorTabs1.java
@@ -0,0 +1,30 @@
+package swing2;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ColorTabs1 {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Color Tabs");
+
+        JTabbedPane tab = new JTabbedPane();
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(Color.RED);
+
+        JPanel p2 = new JPanel();
+        p2.setBackground(Color.BLUE);
+
+        JPanel p3 = new JPanel();
+        p3.setBackground(Color.GREEN);
+
+        tab.addTab("RED", p1);
+        tab.addTab("BLUE", p2);
+        tab.addTab("GREEN", p3);
+
+        f.add(tab);
+        f.setSize(400, 300);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
