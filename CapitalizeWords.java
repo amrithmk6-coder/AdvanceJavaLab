da48d2228a61d5107505a11df1c2fc001046de81
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-20 22:17:44 +0530

    Add files via upload

---
+++ CapitalizeWords.java
@@ -0,0 +1,20 @@
+package capital;
+
+public class CapitalizeWords {
+    static String capitalizeWords(String str) {
+        String[] words = str.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            if (word.length() > 0) {
+                result += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
+            }
+        }
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+        String str = "java is easy to learn";
+        System.out.println(capitalizeWords(str));
+    }
+}
