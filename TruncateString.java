c782e080798c91dcad67bf782298aa0fbd54229c
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-21 11:16:07 +0530

    Update TruncateString.java

--- TruncateString.java
+++ TruncateString.java
@@ -1,3 +1,7 @@
+/**
+Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()
+    **/
 package capital;
 
 public class TruncateString {
