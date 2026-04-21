d5f7ce6d0bceb50caf979dc099b120609808d2eb
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-21 11:16:48 +0530

    Update IsNumeric.java

--- IsNumeric.java
+++ IsNumeric.java
@@ -1,3 +1,7 @@
+/**
+Write a Java Program for Verifying if a string contains only numeric characters using user defined
+function isNumeric()
+    **/
 package capital;
 public class IsNumeric {
     static boolean isNumeric(String str) {
