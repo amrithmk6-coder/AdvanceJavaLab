fd1bebc3ab9006dbbf40cbff4d7be21a956b88ee
amrithmk6-coder <amrithmk6@gmail.com>
2026-04-21 11:14:48 +0530

    Update RemoveWhitespace.java

--- RemoveWhitespace.java
+++ RemoveWhitespace.java
@@ -1,3 +1,7 @@
+/**
+Write a Java Program for Eliminating all whitespace characters from a string using user defined
+function removeWhitespace()
+    **/
 package nullempty;
 public class RemoveWhitespace {
     static String removeWhitespace(String str) {
